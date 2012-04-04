d3516c141aaa170a491241ce56c6b7f02c47e1dc
Iain Wilson <iain.iw.wilson@gmail.com>
2012-04-04 17:05:41 +0100

    Adjustments to feed sanitizing

--- app/helpers/feed_helper.rb
+++ app/helpers/feed_helper.rb
@@ -1,7 +1,11 @@
 module FeedHelper
   
+  def feed_clean(content)
+    Sanitize.clean(content, :elements => ['a', 'p', 'span', 'div', 'strong', 'h3'], :attributes => { 'a' => ['href'] }, :remove_contents => true).html_safe
+  end
+  
   def feed_entry_summary(entry)
-    Sanitize.clean(entry.summary.summarize(:ratio => 10), :elements => ['a', 'p', 'span', 'div', 'strong', 'h3'], :remove_contents => true).html_safe
+    feed_clean(entry.summary).summarize(:ratio => 10).force_encoding('UTF-8')
   end
   
 end
